1814c161a57ce7d80071770b149575e0a4f84406
Esther <esther191299@gmail.com>
2021-11-03 10:35:30 +0100

    Initial commit - Spring boot project set up

---
+++ src/test/java/com/west/assignment/prime_number_calculator/PrimeNumberCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.west.assignment.prime_number_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeNumberCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
