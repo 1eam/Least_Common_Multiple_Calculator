258a0724c748300bdf6765557c27800f8cfee369
Esther <esther191299@gmail.com>
2021-11-03 23:01:25 +0100

    Updated endpointexample in run-instructions

---
+++ src/main/java/com/west/assignment/least_common_multiple_calculator/service/LCMCalculator.java
@@ -0,0 +1,13 @@
+package com.west.assignment.least_common_multiple_calculator.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LCMFromOneTill {
+    public LCMFromOneTill() {
+    }
+
+    public String LCMFromOneTill(int i) {
+        return "2";
+    }
+}
