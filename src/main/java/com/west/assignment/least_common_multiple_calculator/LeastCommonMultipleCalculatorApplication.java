7137564ba469bface5cef5920eb11925dd490798
Esther <esther191299@gmail.com>
2021-11-03 10:54:43 +0100

    Renamed project from 'Prime Number Calculator' to 'LCM Calculator'

---
+++ src/main/java/com/west/assignment/least_common_multiple_calculator/LeastCommonMultipleCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.west.assignment.least_common_multiple_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeastCommonMultipleCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeastCommonMultipleCalculatorApplication.class, args);
+    }
+
+}
