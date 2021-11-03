703f25a42ded881cc4b5cf6e3890139d83819c72
Esther <esther191299@gmail.com>
2021-11-03 21:03:41 +0100

    Updated RESTservice stub

--- src/main/java/com/west/assignment/least_common_multiple_calculator/controller/Controller.java
+++ src/main/java/com/west/assignment/least_common_multiple_calculator/controller/Controller.java
@@ -1,13 +1,16 @@
 package com.west.assignment.least_common_multiple_calculator.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 
-    @GetMapping("/getLeastCommonMultiple")
-    public String getLeastCommonMultiple(){
-        return "2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.";
+    @GetMapping("/LeastCommonMultiple")
+    public String LeastCommonMultipleTill(@RequestParam("till") String userEntry){
+        String LCM = "";
+        return LCM + " is the smallest number that can be divided by each of the numbers from 1 to " + userEntry + " without any remainder.";
     }
+
 }
