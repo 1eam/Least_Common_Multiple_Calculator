67b1f96bc77aa023212c7451b9d0dbbf73702452
Esther <esther191299@gmail.com>
2021-11-04 03:02:19 +0100

    Able to access Hashtable values set up

--- src/main/java/com/west/assignment/least_common_multiple_calculator/controller/Controller.java
+++ src/main/java/com/west/assignment/least_common_multiple_calculator/controller/Controller.java
@@ -1,6 +1,7 @@
 package com.west.assignment.least_common_multiple_calculator.controller;
 
 import com.west.assignment.least_common_multiple_calculator.service.LCMCalculator;
+import com.west.assignment.least_common_multiple_calculator.startup.HashTableFiller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,14 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
 
     private final LCMCalculator lcmCalculator;
+    private final HashTableFiller hashTableFiller;
 
-    public Controller(LCMCalculator lcmCalculator) {
+    public Controller(LCMCalculator lcmCalculator, HashTableFiller hashTableFiller) {
         this.lcmCalculator = lcmCalculator;
+        this.hashTableFiller = hashTableFiller;
     }
 
     @GetMapping("/LeastCommonMultiple")
     public String LeastCommonMultipleTill(@RequestParam("till") String userEntry){
-        String LCM = lcmCalculator.fromOneTill(2);
+        String LCM = hashTableFiller.hashMap.get(userEntry);
         return LCM + " is the smallest number that can be divided by each of the numbers from 1 to " + userEntry + " without any remainder.";
 
 //TODO  //At method-call check the result in the Hashtable & return immediately (O)1
