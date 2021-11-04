9c812cf44c9e7b8ea8c51a6d0b5967e328ced834
Esther <esther191299@gmail.com>
2021-11-04 21:43:12 +0100

    added more One-Till Range-tests

--- src/test/java/com/west/assignment/least_common_multiple_calculator/service/LCMTillCalculatorTest.java
+++ src/test/java/com/west/assignment/least_common_multiple_calculator/service/LCMTillCalculatorTest.java
@@ -5,13 +5,48 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LCMTillCalculatorTest {
+    //One till range tests
     @Test
-    void ResultOfOneTill2(){
+    void ResultOfOneTill_1(){
+        LCMCalculator lcmCalculator = new LCMCalculator();
+        String result = lcmCalculator.fromOneTill(1);
+        assertEquals("1", result);
+
+    }
+
+    @Test
+    void ResultOfOneTill_2(){
         LCMCalculator lcmCalculator = new LCMCalculator();
         String result = lcmCalculator.fromOneTill(2);
         assertEquals("2", result);
 
     }
+
+    @Test
+    void ResultOfOneTill_5(){
+        LCMCalculator lcmCalculator = new LCMCalculator();
+        String result = lcmCalculator.fromOneTill(5);
+        assertEquals("60", result);
+
+    }
+
+    @Test
+    void ResultOfOneTill_10(){
+        LCMCalculator lcmCalculator = new LCMCalculator();
+        String result = lcmCalculator.fromOneTill(10);
+        assertEquals("2520", result);
+
+    }
+
+    @Test
+    void ResultOfOneTill_15(){
+        LCMCalculator lcmCalculator = new LCMCalculator();
+        String result = lcmCalculator.fromOneTill(15);
+        assertEquals("360360", result);
+
+    }
+
+    //Random input combinations tests
     @Test
     void ResultOf2_9_10_Is_90(){
         LCMCalculator lcmCalculator = new LCMCalculator();
