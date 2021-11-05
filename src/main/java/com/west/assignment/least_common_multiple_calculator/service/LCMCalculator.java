dfb9ff49457ab5cdea82eac026fb574ef4a26124
Esther <esther191299@gmail.com>
2021-11-05 05:07:40 +0100

    Updated calculateGCD() to return long values. Passing tests: One -ResultOfOneTill_21

--- src/main/java/com/west/assignment/least_common_multiple_calculator/service/LCMCalculator.java
+++ src/main/java/com/west/assignment/least_common_multiple_calculator/service/LCMCalculator.java
@@ -2,7 +2,7 @@ package com.west.assignment.least_common_multiple_calculator.service;
 
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
+import java.math.BigInteger;
 import java.util.HashMap;
 
 @Service
@@ -12,15 +12,14 @@ public class LCMCalculator {
     public String fromOneTill(int i) {
         //range: [1, 2]
         int[] range = calculateRange(i);
-        System.out.println(Arrays.toString(range));
-
         String lcm = caculateLCM(range);
+
         return lcm;
     }
 
     public String caculateLCM(int[] numbers) {
-        int intLCM = numbers[0];
-        int gcd = numbers[0];
+        long intLCM = numbers[0];
+        long gcd = numbers[0];
 
         // Loops through the array and finds GCD
         // Then divides number by GCD to get the LCM
@@ -30,7 +29,7 @@ public class LCMCalculator {
         }
 
         // Cast value to String for storage in Hashtable. String values will be returned to the end-user
-        String lcmAsString = Integer.toString(intLCM);
+        String lcmAsString = Long.toString(intLCM);
         // Before return, store LCM-value in hashtable for future lookup (O)1. With the given input[] as key
         hashMap.put(numbers, lcmAsString);
 
@@ -38,12 +37,9 @@ public class LCMCalculator {
     }
 
     //recursive function to find GCD of two numbers
-    public static int calculateGCD(int number, int lcm){
-        if(lcm == 0){
-            return number;
-        }
+    public static long calculateGCD(int number, long lcm){
 
-        return calculateGCD(lcm, number % lcm);
+        return BigInteger.valueOf(number).gcd(BigInteger.valueOf(lcm)).longValue();
     }
 
     public int[] calculateRange(int num) {
