48801a4644eec8afea70937e631de6bb7486b08f
Esther <esther191299@gmail.com>
2021-11-05 13:32:24 +0100

    renamed Testclass

--- src/test/java/com/west/assignment/least_common_multiple_calculator/service/ErrorsTest.java
+++ src/test/java/com/west/assignment/least_common_multiple_calculator/service/ErrorsTest.java
@@ -4,7 +4,7 @@ import com.west.assignment.least_common_multiple_calculator.controller.Controlle
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-public class ControllerTest {
+public class ErrorsTest {
     //The applications max asked value is 1-42.
     //Anything above will return a wrong integer answer according to https://www.calculator.net/lcm-calculator.html
     //And should therefore return an exception when asked fromOneTill(42+)
