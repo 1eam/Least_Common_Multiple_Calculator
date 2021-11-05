a9752cab8bdae0aa053551ff22c42df8bde35cc0
Esther <esther191299@gmail.com>
2021-11-05 13:22:09 +0100

    refactored RandomInput TestCases

--- src/test/java/com/west/assignment/least_common_multiple_calculator/service/RandomInputCombinationsTest.java
+++ src/test/java/com/west/assignment/least_common_multiple_calculator/service/RandomInputCombinationsTest.java
@@ -7,11 +7,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class RandomInputCombinationsTest {
 
     @Test
-    void ResultOf2_9_10_Is_90(){
+    void LCMOf_2_9_10_Is_90(){
         LCMCalculator lcmCalculator = new LCMCalculator();
-        int[] numbers = {2,9,10};
+        int[] numbers = {2, 9, 10};
         String result = lcmCalculator.caculateLCM(numbers);
         assertEquals("90", result);
+
+    }
+
+    @Test
+    void LCMOf_7_8_19_21_28_4_Is_3192(){
+        LCMCalculator lcmCalculator = new LCMCalculator();
+        int[] numbers = {7, 8, 19, 21, 28, 4};
+        String result = lcmCalculator.caculateLCM(numbers);
+        assertEquals("3192", result);
 
     }
 }
