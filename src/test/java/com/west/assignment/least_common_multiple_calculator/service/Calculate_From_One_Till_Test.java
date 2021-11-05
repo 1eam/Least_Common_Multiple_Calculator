726957003abb5191a238ff12e5619e0164742a55
Esther <esther191299@gmail.com>
2021-11-05 05:05:09 +0100

    Added additional (now failing) tests: for bigger values which dont fit in int (long)

--- src/test/java/com/west/assignment/least_common_multiple_calculator/service/Calculate_From_One_Till_Test.java
+++ src/test/java/com/west/assignment/least_common_multiple_calculator/service/Calculate_From_One_Till_Test.java
@@ -46,4 +46,38 @@ public class Calculate_From_One_Till_Test {
 
     }
 
+    //Bigger number tests: Starting from 1-23
+
+    @Test
+    void ResultOfOneTill_21(){
+        LCMCalculator lcmCalculator = new LCMCalculator();
+        String result = lcmCalculator.fromOneTill(21);
+        assertEquals("232792560", result);
+
+    }
+
+    @Test
+    void ResultOfOneTill_22(){
+        LCMCalculator lcmCalculator = new LCMCalculator();
+        String result = lcmCalculator.fromOneTill(22);
+        assertEquals("232792560", result);
+
+    }
+
+    @Test
+    void ResultOfOneTill_23(){
+        LCMCalculator lcmCalculator = new LCMCalculator();
+        String result = lcmCalculator.fromOneTill(23);
+        assertEquals("5354228880", result);
+
+    }
+
+    @Test
+    void ResultOfOneTill_25(){
+        LCMCalculator lcmCalculator = new LCMCalculator();
+        String result = lcmCalculator.fromOneTill(25);
+        assertEquals("26771144400", result);
+
+    }
+
 }
